d98cd1ec4f58ec0594d9b8fb7f18b53fbb19752b
kttyo <16376462+kttyo@users.noreply.github.com>
2023-04-12 19:07:05 +0900

    Add column index

--- data/sql/DDL/create_table_yt_mst_cnl.sql
+++ data/sql/DDL/create_table_yt_mst_cnl.sql
@@ -6,5 +6,6 @@ CREATE TABLE `yt_mst_cnl` (
   `uploads_list` varchar(40) DEFAULT NULL,
   `published_at` date DEFAULT NULL,
   `data_update_date` date DEFAULT NULL,
-  PRIMARY KEY (`channel_id`)
+  PRIMARY KEY (`channel_id`),
+  KEY `idx_published_at` (`published_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- data/sql/DDL/create_table_yt_mst_vid.sql
+++ data/sql/DDL/create_table_yt_mst_vid.sql
@@ -8,5 +8,6 @@ CREATE TABLE `yt_mst_vid` (
   `data_update_date` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`video_id`),
   KEY `fk_channel` (`channel_id`),
+  KEY `idx_published_at` (`published_at`),
   CONSTRAINT `fk_channel` FOREIGN KEY (`channel_id`) REFERENCES `yt_mst_cnl` (`channel_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
